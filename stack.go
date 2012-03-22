bd91bdce2e80e36c1198e2c03673fdf68c798508
Joshua Hawxwell <m@hawx.me>
2012-03-22 19:10:44 +0000

    Wrote a stack

---
+++ stack.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+/*
+ Synopsis:
+
+ stk := NewStack()
+ stk.push(1)
+ stk.push(2)
+ stk.push(3)
+ fmt.Println(stk.String())
+ //=> [1 2 3]
+
+ stk.pop()
+ fmt.Println(stk.String())
+ //=> [1 2]
+
+ stk.clear()
+ fmt.Println(stk.String())
+ //=> []
+ */
+type Stack []interface{}
+
+func (k *Stack) size() int {
+	return len(*k)
+}
+
+func (k *Stack) empty() bool {
+	return len(*k) == 0
+}
+
+func (k *Stack) push(s interface{}) {
+	*k = append(*k, s)
+}
+
+func (k *Stack) pop() interface{} {
+	if k.empty() {
+		return nil
+	}
+	last := len(*k) - 1
+	s := (*k)[last]
+	*k = (*k)[:last]
+	return s
+}
+
+func (k *Stack) top() interface{} {
+	if k.empty() {
+		return nil
+	}
+	last := len(*k) - 1
+	s := (*k)[last]
+	return s
+}
+
+func (k *Stack) clear() {
+	*k = (*k)[0:0]
+}
+
+func (k Stack) String() string {
+	str := "["
+	for i, elem := range k {
+		if i > 0 {
+			str += ", "
+		}
+		str += fmt.Sprint(elem)
+	}
+	return str + "]"
+}
+
+func NewStack() *Stack {
+	return new(Stack)
+}
