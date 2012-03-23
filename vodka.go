9526408ea4fe88f4c69633c1d5836abb1f0b5460
Joshua Hawxwell <m@hawx.me>
2012-03-23 18:21:05 +0000

    Added simple REPL

--- vodka.go
+++ vodka.go
@@ -2,11 +2,39 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"bufio"
 )
 
-func main() {
-	code := "1 2 3 + eq? 'Hello World' \"Hello Josh\" [ dup inc ] 5 times"
-	fmt.Println(Parse(code))
+func toString(bytes []uint8) string {
+	str := ""
+	for _, c := range bytes {
+		str += string(byte(c))
+	}
+	return str
+}
 
-	BareEval("1 2 3 + *")
+func promptLine(prompt string) string {
+	fmt.Print(prompt)
+	r := bufio.NewReader(os.Stdin)
+	l, _, _ := r.ReadLine()
+	return toString(l)
+}
+
+func main() {
+	fmt.Println("Vodka REPL, CTRL+C or type 'quit' to quit")
+
+	stk := NewStack()
+	tbl := BootedTable()
+
+	for {
+		line := promptLine(">> ")
+
+		if line == "quit" {
+			break
+		}
+
+		stk, tbl = Eval(line, stk, tbl)
+		fmt.Printf("=> %s\n", stk.String())
+	}
 }
