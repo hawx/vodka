7dd136ca557f8e7d776356f679b18af1c961c9ee
Joshua Hawxwell <m@hawx.me>
2012-05-04 15:55:25 +0100

    Documentation is now nice to use.

--- rsc/doc.js
+++ rsc/doc.js
@@ -1,8 +1,33 @@
 $(document).ready(function(){
-    $(".summary").jstree({
-        core: {
 
-        },
-        plugins: ["themes", "html_data"]
+    $('.summary ul').addClass('closed');
+
+    $('.summary > li > a').click(function() {
+        $('.summary ul').addClass('closed');
+        var ul = $(this).next();
+        ul.toggleClass("closed");
+    });
+
+    function openLeaf(name) {
+        $('.summary ul').addClass('closed');
+        $('.summary a[href="#x'+name+'"]').next().removeClass('closed');
+    }
+
+    $('.docs > h1').appear(function() {
+        openLeaf($(this).attr('id'));
+    }, {one: false});
+
+
+    var toScroll = $('ul').position().top;
+    $(window).scroll(function() {
+        if (toScroll >= $(window).scrollTop()) {
+            if ($('.summary').hasClass('fixed')) {
+                $('.summary').removeClass('fixed');
+            }
+        } else {
+            if (!$('.summary').hasClass('fixed')) {
+                $('.summary').addClass('fixed');
+            }
+        }
     });
 });
