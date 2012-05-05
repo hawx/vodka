d87008a2dc0cf9025405962baceadb3fab7ecda1
Joshua Hawxwell <m@hawx.me>
2012-05-05 16:26:58 +0100

    Vodka can now take a list of files to run

--- vodka.go
+++ vodka.go
@@ -52,9 +52,12 @@ func main() {
 	if len(os.Args) > 1 {
 		if os.Args[1] == "doc" {
 			Doc([]string{os.Args[2]}, "doc.html")
+
 		} else {
-			contents, _ := ioutil.ReadFile(os.Args[1])
-			BareEval(string(contents))
+			for _, file := range os.Args[1:] {
+				contents, _ := ioutil.ReadFile(file)
+				BareEval(string(contents))
+			}
 		}
 
 	} else {
