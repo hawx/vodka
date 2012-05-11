10306be08823abb98cb33538fdc3063619c8de9b
Joshua Hawxwell <m@hawx.me>
2012-05-11 19:17:57 +0100

    Add method to add Token to Tokens

--- parser.go
+++ parser.go
@@ -24,6 +24,14 @@ func (ts *Tokens) String() string {
 	return str + "]"
 }
 
+func (ts *Tokens) Add(tok Token) {
+	*ts = append(*ts, tok)
+}
+
+func (ts *Tokens) AddToken(key, val string) {
+	ts.Add(NewToken(key, val))
+}
+
 type Token struct {
 	key string
 	val string
@@ -48,53 +56,53 @@ func FullParse(code string) *Tokens {
 			// Ignore whitespace
 
 		case '\n':
-			*list = append(*list, NewToken("newline", ""))
+			list.AddToken("newline", "")
 
 		case ';':
 			i, temp = parseUntil(i, code, '\n')
-			*list = append(*list, NewToken("comment",
-				strings.TrimSpace(strings.TrimLeft(temp, ";"))))
+			list.AddToken("comment",
+				strings.TrimSpace(strings.TrimLeft(temp, ";")))
 
 		case '.':
-			*list = append(*list, NewToken("stm", ""))
+			list.AddToken("stm", "")
 
 		case '\'':
 			i++
 			i, temp = parseUntil(i, code, '\'')
-			*list = append(*list, NewToken("str", temp))
+			list.AddToken("str", temp)
 
 		case '"':
 			i++
 			i, temp = parseUntil(i, code, '"')
-			*list = append(*list, NewToken("str", temp))
+			list.AddToken("str", temp)
 
 		case '[':
 			i++
 			i, temp = parseMatching(i, code, '[', ']')
-			*list = append(*list, NewToken("stm", temp))
+			list.AddToken("stm", temp)
 
 		case ':':
 			i++
 			i, temp = parseUntilWhitespace(i, code)
-			*list = append(*list, NewToken("stm", temp))
+			list.AddToken("stm", temp)
 
 		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
 			i, temp = parseUntilWhitespace(i, code)
-			*list = append(*list, NewToken("int", temp))
+			list.AddToken("int", temp)
 
 		case '-':
 			i, temp = parseUntilWhitespace(i, code)
 			matcher, _ := regexp.Compile("-[0-9]+")
 
 			if matcher.MatchString(temp) {
-				*list = append(*list, NewToken("int", temp))
+				list.AddToken("int", temp)
 			} else {
-				*list = append(*list, NewToken("fun", temp))
+				list.AddToken("fun", temp)
 			}
 
 		default:
 			i, temp = parseUntilWhitespace(i, code)
-			*list = append(*list, NewToken("fun", temp))
+			list.AddToken("fun", temp)
 
 		}
 	}
@@ -107,7 +115,7 @@ func Parse(code string) *Tokens {
 
 	for _, tok := range *FullParse(code) {
 		if tok.key != "comment" && tok.key != "newline" {
-			*list = append(*list, tok)
+			list.Add(tok)
 		}
 	}
 
@@ -153,5 +161,6 @@ func parseMatching(idx int, code string, op, cl uint8) (i int, s string) {
 			str += string(c)
 		}
 	}
+
 	return len(code), strings.TrimSpace(str)
 }
